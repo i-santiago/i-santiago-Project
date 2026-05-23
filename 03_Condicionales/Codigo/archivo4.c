d554898ff64e3ab53732e263b86e05a56094d2da
i-santiago <ivan.gonzalez0196@alumnos.udg.mx>
2026-05-23 13:00:42 -0600

    Create archivo4.c

---
+++ 03_Condicionales/Codigo/archivo4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Ingresa tu numero:\n");
+    scanf("%d", &a);
+    if( (a%2) == 0){
+        printf("Tu numero es par\n");
+    } else {
+        if((a%2) != 0){
+             printf("Tu numero es impar\n");
+
+    }else if (a/a){
+         printf("Tu numero es primo\n");
+    }
+    }
+
+
+
+
+    return 0;
+}
